29b0928a4ab5799d1a7d662979ce7fe45f90cdd1
Vansh <vanshmorwani1@gmail.com>
2026-01-16 01:32:41 +0530

    add cancel function and save function to modal.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
+import AddContentModal from "./components/AddContentModal";
+import { HeaderTitle } from "@react-navigation/elements";
+import colors from "./styles/colors";
+import { Stack } from "expo-router";
+import AddContentButton from "./components/AddContentButton";
 
-export default function Index() {
+interface IndexProps {
+  link?: string;
+  title?: string;
+  isShare?: boolean;
+}
+
+export default function Index(props: IndexProps) {
+  const [title, setTitle] = useState(props.title || "")
+  const [link, setLink] = useState(props.link || "")
+  const [addContent, setAddContent] = useState(props.isShare || false)
+
+  const onCancel = () => {
+    setAddContent(false);
+    setTitle("");
+    setLink("");
+  }
+
+  const onSave = (newTitle: string, newLink: string) => {
+    setAddContent(false);
+    setTitle("");
+    setLink("");
+  }
+
   return (
+    <>
+    <Stack.Screen options={{
+      headerTitle: () => <HeaderTitle>LookLater</HeaderTitle>,
+      headerStyle: {
+        backgroundColor: colors.background,
+      },
+      headerTitleStyle: {
+        color: colors.text,
+      },
+    }} />
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        backgroundColor: colors.background,
+        paddingBottom: 100,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 20 }}>Welcome!</Text>
+      <AddContentModal onCancel={onCancel} onSave={onSave} link={link} title={title} isOpen={addContent} />
+      <AddContentButton onPress={() => setAddContent(true)}></AddContentButton>
     </View>
+    </>
   );
 }
